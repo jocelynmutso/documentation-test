9c03f571f613f9661268345f38be4f01f6d88d55
Jocelyn Mutso (ext) <jocelyn.mutso@resys.io>
2020-12-22 20:09:42 +0200

    theme test

--- src/components/DialobNav.js
+++ src/components/DialobNav.js
@@ -2,22 +2,18 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import DialobIntro from './DialobIntro';
-import setNowShowing from './DialobDoc';
 
 
-
-export default function DialobNav(props) {
-  const { classes, thisWillHappenOnClickingTheTextInTheNavBar } = props;
+export default function DialobNav({ classes }) {
 
   return (
     <AppBar position="fixed" className={classes.appBar}>
       <Toolbar>
-        <Typography variant="h6" noWrap onClick={thisWillHappenOnClickingTheTextInTheNavBar}>
-         Documentation
-        </Typography>
+        <Typography variant="h6" noWrap>
+        
+          Composer Documentation
+          </Typography>
       </Toolbar>
     </AppBar>
   );
 }
-
--- src/themes/DefaultTheme.tsx
+++ src/themes/DefaultTheme.tsx
@@ -2,8 +2,19 @@ import { createMuiTheme } from '@material-ui/core/styles';
 import { PaletteOptions } from '@material-ui/core/styles/createPalette';
 
 const palette: PaletteOptions = {
-  type: 'dark'
-}
+    type: 'light',
+    primary: {
+      main: '#3E92CC',
+    },
+    secondary: {
+      main: '#ff8600',
+    },
+    text: {
+      primary: 'rgba(9,9,59,0.87)',
+      secondary: 'rgba(44,75,148,0.75)',
+    },
+  }
+
 
 const DefaultTheme = createMuiTheme({
   palette: palette,
